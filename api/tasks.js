40e1aa49cf576e23b250f07b558274d312ae06a7
tabularasae <tabularasae@icloud.com>
2025-06-27 18:28:51 -0400

    Updated tasks.js router and updated db schema

--- api/tasks.js
+++ api/tasks.js
@@ -7,10 +7,81 @@ const { Task, User } = require("../database");
 
 // GET all tasks
 router.get("/", async (req, res) => {
-  // Replace this with your code!
-  res.status(501).send("Not implemented");
+  try {
+    // Replace this with your code!
+    const tasks = await Task.findAll();
+    // const users = User.findAll();
+    // res.send(users)
+    res.send(tasks);
+    // res.status(501).send("Not implemented");
+  } catch (error) {
+    console.error("Failed to find all", error);
+    res.sendStatus(404);
+  }
 });
 
+// router.get("/users", async (req, res) => {
+//   try {
+//     // Replace this with your code!
+//     // const tasks = await Task.findAll();
+//     const users = User.findAll();
+//     // res.send(users)
+//     res.send(users);
+//     // res.status(501).send("Not implemented");
+//   } catch (error) {
+//     console.error("Failed to find all", error);
+//     res.sendStatus(404);
+//   }
+// });
+
+// GET a single task by id
+router.get("/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const task = Task.findByPk(id);
+    res.send(task);
+  } catch (error) {
+    console.error("Failed to find the specific task you are lookng for");
+    res.sendStatus(404);
+  }
+});
+
+// Patch a task by id
+router.patch("/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const changes = req.body;
+    Task.update(id, changes);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Failed to update the specific task", error);
+    res.sendStatus(404);
+  }
+});
+
+// Delete a task by id
+router.delete("/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    Task.delete(id);
+  } catch (error) {
+    console.error("Failed to delete the task", error);
+    res.sendStatus(404);
+  }
+});
+
+// Create a new task
+router.post("/", (req, res) => {
+  try {
+    const task = req.body;
+    Task.create(task);
+    res.sendStatus(201);
+  } catch (error) {
+    console.error("Failed to create a new task", error);
+    res.sendStatus(404);
+  }
+});
+module.exports = router;
 // GET a single task by id
 
 // Patch a task by id
--- database/task.js
+++ database/task.js
@@ -3,6 +3,29 @@ const db = require("./db");
 
 // TASK 1: Define the Task model here
 const Task = db.define("task", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+
+  // title: "Get eight hours of sleep",
+  // description: "Sleepy time tea is a must",
+  // completed: false,
+  // userId: users[0].id,
+
   // You should define the following columns:
   // - title: string, required
   // - description: string, required
